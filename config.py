3d1590f8ece6e47520d43431d0c038c1676a7f31
Hardik44 <jhardik4445@gmail.com>
2021-03-19 20:12:58 +0530

    add db

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:abc123@localhost/imdb"
+    SQLALCHEMY_DATABASE_URI = "postgres://qfuuwyxuzgstli:f5513d10f9df03d0f8d396d73b385ab0727de3720a364d43238e8db3856c8c8e@ec2-18-211-97-89.compute-1.amazonaws.com:5432/df2ojcvl26lh56"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     SECRET_KEY = "c4llKiB7g-pIUwukcJNgACmTfkVFGmJnzbVxcEY2Hms"
